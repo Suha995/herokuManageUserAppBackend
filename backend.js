812503a58d797d89c0b0dc0d5082e8c5befa1cfd
Suhair Aljammal <saljammal995@gmail.com>
2021-11-18 14:12:30 +0100

    fix endpoints

--- backend.js
+++ backend.js
@@ -43,7 +43,7 @@ app.delete("/deleteuser/:id", (req, res) => {
   });
 });
 
-app.post("/adduser", (req, res) => {
+app.post("/insertuser", (req, res) => {
   const user = req.body.user;
   execMongo(async (db) => {
     const insertResult = await db.collection('users100').insertOne(user);
@@ -53,7 +53,7 @@ app.post("/adduser", (req, res) => {
   });
 });
 
-app.patch("/edituser/:id", (req, res) => {
+app.patch("/edituseremail/:id", (req, res) => {
     const email = req.body.email;
     const id = req.params.id;
     execMongo(async (db) => {
